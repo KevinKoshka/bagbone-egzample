0c76321605dad78ebbdb9e248afc76c6c15051b3
unknown <kevin.coscarelli@GLB-D3059.globant.com>
2015-05-11 17:01:18 -0300

    More stuff added. Read more about Backbone.sync

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+//---------------VIEW-----------------
+
 SearchView = Backbone.View.extend({
 	//Funcion que se ejecuta al principio.
 	initialize: function(){
@@ -28,3 +30,75 @@ SearchView = Backbone.View.extend({
 });
 
 var search_view = new SearchView({el: $("#search_container")});
+
+//---------------MODEL-----------------
+//Los modelos se usan para representar datos del server, y las acciones
+//que se realicen en ellas se traducirán en operaciones RESTful.
+//El atrubito "id" identifica como encontrar el modelo en una base de datos
+//
+
+//Clase Person, base de los modelos. Como las views, tmb se puede
+//setear la función initialize, que se ejecuta al instanciar la clase.
+var Person = Backbone.Model.extend({
+	initialize: function(){
+		alert('Welcome to this world!');
+
+		//Todos los atributos de un modelo pueden tener listeners
+		//asociados para detectar cambios en los valores.
+		this.on("change:species", function(model){
+			if(model.get("species") === "Martian"){
+				alert(model.get("name") + " just went Martian!");
+			}
+		});
+	},
+
+	//Se pueden definir propiedades por default.
+	defaults: {
+		name: "Fetus",
+		age: 0,
+		species: "Homo Sapiens Sapiens",
+	},
+
+	//Funciones de la clase.
+	goMartian: function(){
+		this.set({species: "Martian"});
+	},
+});
+
+//Las propiedades de instancia se pueden setear en la misma creación...
+var person1 = new Person({name:"Thomas", age:123});
+//...O mediante model.set().
+var person2 = new Person();
+person2.set({name:"Carlos", age:321});
+
+
+//Se obtienen los valores de las propiedades mediante model.get().
+var age1 = (person1.get("name")) + ' ' + (person2.get("age"));
+console.log(age1);
+
+person2.goMartian();
+
+
+var UserModel = Backbone.Model.extend({
+	urlRoot: "user",
+	defaults:{
+		name: '',
+		email: ''
+	},
+});
+
+var user = new UserModel();
+var userDetails = {
+	name: 'Thomas',
+	email: 'gofuckyourself@dickbutt.com'
+};
+
+user.save();
+
+user.save(userDetails, {
+	success: function(user){
+		alert(user.toJSON());
+		console.log(1);
+	}
+});
+
